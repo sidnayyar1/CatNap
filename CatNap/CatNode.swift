d6d0f4126f9e86ae348ffe5eafc13f163e6a3a32
sidharth nayyar <sidnayyar1@gmail.com>
2020-06-22 13:03:14 -0400

    [Sid] Added nodes and protocols to the game connected them with swift files

---
+++ CatNap/CatNode.swift
@@ -0,0 +1,6 @@
+import SpriteKit
+class CatNode: SKSpriteNode, EventListenerNode {
+  func didMoveToScene() {
+    print("cat added to scene")
+  }
+}
--- CatNap/GameScene.swift
+++ CatNap/GameScene.swift
@@ -1,89 +1,31 @@
-//
-//  GameScene.swift
-//  CatNap
-//
-//  Created by Sidharth Nayyar on 6/22/20.
-//  Copyright © 2020 Sidharth Nayyar. All rights reserved.
-//
-
 import SpriteKit
-import GameplayKit
-
 class GameScene: SKScene {
+    var bedNode: BedNode!
+    var catNode: CatNode!
+   // var catNode:
+  override func didMove(to view: SKView) {
+    // Calculate playable margin
+   
+    let maxAspectRatio: CGFloat = 16.0/9.0
+    let maxAspectRatioHeight = size.width / maxAspectRatio
+    let playableMargin: CGFloat = (size.height
+      - maxAspectRatioHeight)/2
+    let playableRect = CGRect(x: 0, y: playableMargin,
+      width: size.width, height: size.height-playableMargin*2)
+    physicsBody = SKPhysicsBody(edgeLoopFrom: playableRect)
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    enumerateChildNodes(withName: "//*", using: { node, _ in
+      if let eventListenerNode = node as? EventListenerNode {
+        eventListenerNode.didMoveToScene()
+      }
+    })
+    bedNode = childNode(withName: "bed") as! BedNode
+       catNode = childNode(withName: "//cat_body") as! CatNode
     
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+     bedNode.setScale(1.5)
+    catNode.setScale(1.5)
+  }
+}
+protocol EventListenerNode {
+  func didMoveToScene()
 }
